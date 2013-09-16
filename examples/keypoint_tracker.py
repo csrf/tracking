165174978ead445f8f999130aad9d8628303db12
Rich Wareham <rjw57@cam.ac.uk>
2013-09-16 17:32:48 +0100

    update tracker example to have more control over output

--- examples/keypoint_tracker.py
+++ examples/keypoint_tracker.py
@@ -8,7 +8,13 @@ Usage:
 Options:
     --no-video                  Do not show video in output.
     --max-position-sigma=SIGMA  Do not show positions with >SIGMA error in location.
-    --max-velocity-sigma=SIGMA  Do not show positions with >SIGMA error in velocity.
+    --max-velocity-sigma=SIGMA  Do not show velocity with >SIGMA error in velocity.
+    --max-frames=COUNT          If specified, only process at most COUNT frames.
+    --no-cluster                Do not attempt clustering.
+    --no-show-states            Do not show tracked states.
+    --show-kps                  Show keypoints per frame.
+    --trail-length=FRAMES       Show track trails with length FRAMES.
+                                [default: 0]
 
 Input video can be anything OpenCV can read. Output video is AVI. Keypoints are
 read from an HDF5 file.
@@ -38,7 +44,7 @@ def main():
     video_writer = None
     clusters = []
 
-    while True:
+    while options['--max-frames'] is None or frame_idx < int(options['--max-frames']):
         # Read in frame image
         rv, frame = cap.read()
         frame_idx += 1
@@ -81,6 +87,11 @@ def main():
 
         # Construct a list of key points
         kps = list(Keypoint(frame_idx, loc[:2], loc[2], desc) for loc, desc in zip(kp_im_locs, kp_descs))
+
+        if options['--show-kps']:
+            for kp in kps:
+                x, y = kp.location
+                cv2.circle(output_frame, (int(x), int(y)), 5, (0,0,200), lineType=cv2.CV_AA)
         
         # Track this frame's keypoints
         tracking.add_frame(frame_pair[0], frame_pair[1], frame_idx, kps)
@@ -93,78 +104,94 @@ def main():
             frame_states.append(t.states[frame_idx - t.initial_frame_idx])
             frame_covars.append(t.covariances[frame_idx - t.initial_frame_idx].copy())
             frame_track_kps.append(t.associated_keypoints[-1])
+
+        # Draw trails if required
+        trail_length = int(options['--trail-length'])
+        if trail_length > 0:
+            for t in tracking.tracks:
+                if t.final_frame_idx <= frame_idx - trail_length or t.initial_frame_idx > frame_idx:
+                    continue
+
+                start_frame = frame_idx - trail_length + 1
+                start_idx = start_frame - t.initial_frame_idx
+
+                for s1, s2 in zip(t.states[start_idx:-1], t.states[start_idx+1:]):
+                    cv2.line(output_frame, (int(s1[0]), int(s1[1])),
+                            (int(s2[0]), int(s2[1])), (200,0,200),
+                            lineType=cv2.CV_AA)
             
         # Convert states to an array
         frame_states = np.array(frame_states)
 
-        # PDF of choosing kp uniformly from image
-        h, w = frame.shape[:2]
-        non_cluster_pdf = -30
+        if not options['--no-cluster']:
+            # PDF of choosing kp uniformly from image
+            h, w = frame.shape[:2]
+            non_cluster_pdf = -30
 
-        # Best existing cluster for each state and the associated PDF
-        state_association = [(-1, non_cluster_pdf),] * frame_states.shape[0]
+            # Best existing cluster for each state and the associated PDF
+            state_association = [(-1, non_cluster_pdf),] * frame_states.shape[0]
 
-        # PDF of choosing states from each active cluster
-        for c_idx, cluster in enumerate(clusters):
-            # skip elderly clusters
-            if cluster.last_update_frame_idx != frame_idx - 1:
-                continue
-
-            cluster_mu, cluster_sigma = cluster.predict(frame_idx)
-
-            for s_idx in xrange(len(state_association)):
-                s = frame_states[s_idx,:]
-                c = frame_covars[s_idx]
-                _, current_pdf = state_association[s_idx]
-
-                pdf = mv_gaussian_log_pdf(s, cluster_mu, cluster_sigma + c)[0]
-                if pdf > current_pdf:
-                    state_association[s_idx] = (c_idx, pdf)
-
-        # Go through associations
-        unassigned_states, unassigned_covars = [], []
-        cluster_states = [None,] * len(clusters)
-        for s, c, assoc in zip(frame_states, frame_covars, state_association):
-            c_idx = assoc[0]
-            if c_idx < 0:
-                unassigned_states.append(s)
-                unassigned_covars.append(c)
-                continue
-            
-            if cluster_states[c_idx] is None:
-                cluster_states[c_idx] = [(s, c)]
-            else:
-                cluster_states[c_idx].append((s, c))
-
-        for cluster, assignment in zip(clusters, cluster_states):
-            if assignment is None:
-                if cluster.final_frame_idx >= frame_idx - 3:
-                    cluster.update(frame_idx)
-            else:
-                states = np.array(list(s for s,c in assignment))
-
-                if states.shape[0] >= 2:
-                    sigma = np.cov(states.T)
-                else:
-                    sigma = cluster.covariances[-1].copy()
-
-                mu = np.mean(states, axis=0)
-                for _, cov in assignment:
-                    sigma += cov
-
-                cluster.update(frame_idx, mu, sigma)
-
-                minx, maxx = states[:,0].min(), states[:,0].max()
-                miny, maxy = states[:,1].min(), states[:,1].max()
-
-                if maxx - minx > 300 or maxy - miny > 300:
+            # PDF of choosing states from each active cluster
+            for c_idx, cluster in enumerate(clusters):
+                # skip elderly clusters
+                if cluster.last_update_frame_idx != frame_idx - 1:
                     continue
 
-                cv2.rectangle(output_frame,
-                        (int(minx), int(miny)), (int(maxx), int(maxy)), (0,0,200), lineType=cv2.CV_AA)
+                cluster_mu, cluster_sigma = cluster.predict(frame_idx)
 
-                state, cov = cluster.predict(frame_idx)
-                draw_cov(output_frame, cov[:2,:2], state[:2], (0,0,200), lineType=cv2.CV_AA)
+                for s_idx in xrange(len(state_association)):
+                    s = frame_states[s_idx,:]
+                    c = frame_covars[s_idx]
+                    _, current_pdf = state_association[s_idx]
+
+                    pdf = mv_gaussian_log_pdf(s, cluster_mu, cluster_sigma + c)[0]
+                    if pdf > current_pdf:
+                        state_association[s_idx] = (c_idx, pdf)
+
+            # Go through associations
+            unassigned_states, unassigned_covars = [], []
+            cluster_states = [None,] * len(clusters)
+            for s, c, assoc in zip(frame_states, frame_covars, state_association):
+                c_idx = assoc[0]
+                if c_idx < 0:
+                    unassigned_states.append(s)
+                    unassigned_covars.append(c)
+                    continue
+                
+                if cluster_states[c_idx] is None:
+                    cluster_states[c_idx] = [(s, c)]
+                else:
+                    cluster_states[c_idx].append((s, c))
+
+            for cluster, assignment in zip(clusters, cluster_states):
+                if assignment is None:
+                    if cluster.final_frame_idx >= frame_idx - 3:
+                        cluster.update(frame_idx)
+                else:
+                    states = np.array(list(s for s,c in assignment))
+
+                    if states.shape[0] >= 2:
+                        sigma = np.cov(states.T)
+                    else:
+                        sigma = cluster.covariances[-1].copy()
+
+                    mu = np.mean(states, axis=0)
+                    for _, cov in assignment:
+                        sigma += cov
+
+                    cluster.update(frame_idx, mu, sigma)
+
+                    minx, maxx = states[:,0].min(), states[:,0].max()
+                    miny, maxy = states[:,1].min(), states[:,1].max()
+
+                    if maxx - minx > 300 or maxy - miny > 300:
+                        continue
+
+                    cv2.rectangle(output_frame,
+                            (int(minx), int(miny)), (int(maxx), int(maxy)), (0,0,200), lineType=cv2.CV_AA)
+
+                    state, cov = cluster.predict(frame_idx)
+                    draw_cov(output_frame, cov[:2,:2], state[:2], (0,0,200), lineType=cv2.CV_AA)
 
         # Draw 'o' over each frame state
         sc = 10.0
@@ -196,15 +223,16 @@ def main():
             filtered_states.append(s)
             filtered_covs.append(c)
 
-            draw_cov(output_frame, c[:2,:2], s[:2], (255,0,0), lineType=cv2.CV_AA)
-            cv2.line(output_frame, (int(s[0]), int(s[1])), (int(s[0]+sc*s[2]),
-                int(s[1]+sc*s[3])), (0,200,0), lineType=cv2.CV_AA)
-            draw_cov(output_frame, sc*sc*c[2:4,2:4], s[:2]+sc*s[2:4], (0,200,0), lineType=cv2.CV_AA)
+            if not options['--no-show-states']:
+                draw_cov(output_frame, c[:2,:2], s[:2], (255,0,0), lineType=cv2.CV_AA)
+                cv2.line(output_frame, (int(s[0]), int(s[1])), (int(s[0]+sc*s[2]),
+                    int(s[1]+sc*s[3])), (0,200,0), lineType=cv2.CV_AA)
+                draw_cov(output_frame, sc*sc*c[2:4,2:4], s[:2]+sc*s[2:4], (0,200,0), lineType=cv2.CV_AA)
 
         filtered_states = np.array(filtered_states)
 
         # Cluster unlabelled states
-        if len(unassigned_states) > 4:
+        if not options['--no-cluster'] and len(unassigned_states) > 4:
             cluster_states = np.copy(np.array(unassigned_states))
             cluster_covs = list(unassigned_covars)
 
